5986808a657dd5205b1f8dcbeac4e8a0744707da
Adam Catto <agocatto@gmail.com>
2015-09-26 20:42:38 -0400

    Create insertion_sort.py
    Insertion Sort algorithm implemented in Python

---
+++ chapter_1/insertion_sort.py
@@ -0,0 +1,19 @@
+####    Author: Adam Catto                           ####
+####    Date: Sept.26 2015                           ####
+####    git: acatto/sadme/chapter_1/insertionsort    ####
+####    written in python                            ####
+
+
+
+
+    
+def InsertionSort(lyst):
+    
+    n = len(lyst)        
+    for i in range(0, n-1):
+        j = i
+        while j>=0 and lyst[j+1] < lyst[j]:
+            lyst[j+1], lyst[j] = lyst[j], lyst[j+1]
+            j -= 1
+    
+    return lyst
